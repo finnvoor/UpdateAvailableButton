22adfdf32d5681c2467b502bb7a49207d83b4d2b
Finn Voorhees <finnvoorhees@gmail.com>
2023-07-12 23:07:15 +0100

    UpdateAvailableButton

--- Package.swift
+++ Package.swift
@@ -1,23 +1,21 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "UpdateAvailableButton",
+    platforms: [.iOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UpdateAvailableButton",
-            targets: ["UpdateAvailableButton"]),
+            targets: ["UpdateAvailableButton"]
+        ),
     ],
+    dependencies: [.package(url: "https://github.com/Finnvoor/iTunesSearchAPI.git", branch: "main")],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UpdateAvailableButton"),
-        .testTarget(
-            name: "UpdateAvailableButtonTests",
-            dependencies: ["UpdateAvailableButton"]),
+            name: "UpdateAvailableButton",
+            dependencies: [.product(name: "iTunesSearchAPI", package: "iTunesSearchAPI")]
+        )
     ]
 )
--- Sources/UpdateAvailableButton/UpdateAvailableButton.swift
+++ Sources/UpdateAvailableButton/UpdateAvailableButton.swift
@@ -1,2 +1,75 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import iTunesSearchAPI
+import SwiftUI
+
+// MARK: - UpdateAvailableButton
+
+public struct UpdateAvailableButton: View {
+    // MARK: Lifecycle
+
+    public init(
+        appID: Int,
+        localVersion: String? = Bundle.main.object(
+            forInfoDictionaryKey: "CFBundleShortVersionString"
+        ) as? String
+    ) {
+        self.appID = appID
+        self.localVersion = localVersion
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        ZStack {
+            if updateAvailable {
+                Link(destination: URL(string: "https://apps.apple.com/app/id\(appID)")!) {
+                    Label("Update Available", systemImage: "sparkles")
+                        .font(.caption2)
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+            }
+        }
+        .task { await checkForUpdates() }
+    }
+
+    // MARK: Private
+
+    private let appID: Int
+    private let localVersion: String?
+
+    @State private var updateAvailable = false
+
+    private func checkForUpdates() async {
+        guard let localVersion,
+              let result = try? await iTunesSearchAPI.lookup(
+                  iTunesID: appID
+              ).results.first,
+              let remoteVersion = result.version else { return }
+
+        var localVersionComponents = localVersion.components(separatedBy: ".")
+        var remoteVersionComponents = remoteVersion.components(separatedBy: ".")
+        let zeroDiff = localVersionComponents.count - remoteVersionComponents.count
+        if zeroDiff == 0 {
+            withAnimation {
+                updateAvailable = localVersion
+                    .compare(remoteVersion, options: .numeric) == .orderedAscending
+            }
+        } else {
+            let zeros = Array(repeating: "0", count: abs(zeroDiff))
+            if zeroDiff > 0 {
+                remoteVersionComponents.append(contentsOf: zeros)
+            } else {
+                localVersionComponents.append(contentsOf: zeros)
+            }
+            withAnimation {
+                updateAvailable = localVersionComponents.joined(separator: ".")
+                    .compare(remoteVersionComponents.joined(separator: "."), options: .numeric) == .orderedAscending
+            }
+        }
+    }
+}
+
+#Preview {
+    UpdateAvailableButton(appID: 1673518618, localVersion: "1.0.0")
+}
